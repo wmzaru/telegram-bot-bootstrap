386e4efc254f61cefe9292d74e0f81a3c45ee944
jerroldwee <jerrold.wee.2011@smu.edu.sg>
2015-10-29 19:46:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,48 @@
 
-var _ = require('lomath');
-var express = require('express');
-var app = express();
-// express middlewares
-var morgan = require('morgan');
-var ejs = require('ejs');
-var bodyParser = require('body-parser');
-var multer = require('multer');
+ // dependencies
+ // express
+ var _ = require('lomath');
+ var express = require('express');
+ var app = express();
+ // express middlewares
+ var morgan = require('morgan');
+ var ejs = require('ejs');
+  var bodyParser = require('body-parser');
+ var multer = require('multer');
 
-// telegram bot
-var bot = require(__dirname + '/bot.js');
-var token = '154392636:AAFAopfSVBIZJTyTdLnzAe0x_UvNbRdO_JE';
-var webhookUrl = 'https://waterflow.herokuapp.com/'
-var bot1 = new bot(process.env.TOKEN || token, process.env.WEBHOOK || webhookUrl);
-
-
-// engine to render HTML
-app.engine('.html', ejs.__express);
-app.set('view engine', 'html');
-// set the port number
-app.set('port', process.env.PORT || 8443);
+ // telegram bot
+ var bot = require(__dirname + '/bot.js');
+ var token = '154392636:AAFAopfSVBIZJTyTdLnzAe0x_UvNbRdO_JE';
+ var webhookUrl = 'https://waterflow.herokuapp.com/'
+ var bot1 = new bot(process.env.TOKEN || token, process.env.WEBHOOK || webhookUrl);
 
 
-app.use(express.static(__dirname + '/views'));
+ // engine to render HTML
+ app.engine('.html', ejs.__express);
+ app.set('view engine', 'html');
+ // set the port number
+ app.set('port', process.env.PORT || 8443);
 
-app.use(bodyParser.json());
-app.use(multer());
+ // Mount middlewares defaulted for root: 
+ // log all HTTP calls for debugging
+ // app.use(morgan('combined'));
+ // use resources for html: HTML, JS and CSS etc.
+ app.use(express.static(__dirname + '/views'));
+ // parse incoming formData into JSON
+ app.use(bodyParser.json());
+ app.use(multer());
 
 
-
-app.route('/')
+ // route: concise way to group all HTTP methods for a path
+ app.route('/')
      .get(function(req, res) {
          // console.log("you GET")
          res.render('index')
      })
      .post(function(req, res) {
-
+         // send back to end req-res cycle
          res.json('okay, received\n');
-
+         // robot handle as middleware for POST
          bot1.handle(req, res)
      })
      .put(function(req, res) {
@@ -45,7 +50,7 @@ app.route('/')
      })
 
 
-
+ // finally, listen to the specific port for any calls
  app.listen(app.get('port'), function() {
      console.log('Node app is running on port', app.get('port'));
  });
